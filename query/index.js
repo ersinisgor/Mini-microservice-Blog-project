1337f30f035dc36db632f9dba7a766082bd9dcbe
Ersin Isgor <ersinisgor@hotmail.com>
2025-04-23 06:50:07 +0300

    feat: implement event handling for post and comment creation

--- query/index.js
+++ query/index.js
@@ -6,9 +6,27 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const posts = {};
+
 app.get("/posts", (req, res) => {});
 
-app.post("/events", (req, res) => {});
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+
+    post.comments.push({ id, content });
+  }
+
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log("Query service is running on port 4002");
