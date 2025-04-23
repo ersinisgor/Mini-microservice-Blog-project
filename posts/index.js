9dce8aa6d39cf31ee4e24b38adc536e3bbb6a138
Ersin Isgor <ersinisgor@hotmail.com>
2025-04-23 06:08:14 +0300

    feat: add event handling endpoint to posts and comments services

--- posts/index.js
+++ posts/index.js
@@ -31,6 +31,12 @@ app.post("/posts", async (req, res) => {
   res.status(201).send(posts[id]);
 });
 
+app.post("/events", (req, res) => {
+  console.log("Event received: ", req.body.type);
+
+  res.send({});
+});
+
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
